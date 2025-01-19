723d10fb7c406c4192a7d1533bd66deba28fff5b
Siddendra Kodepalli <siddendra@gmail.com>
2025-01-19 16:39:53 +0530

    code refactor

--- springai-gpt-4o-demo/src/main/java/com/example/springaigpt4odemo/SpringaiGpt4oDemoApplication.java
+++ springai-gpt-4o-demo/src/main/java/com/example/springaigpt4odemo/SpringaiGpt4oDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringaiGpt4oDemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringaiGpt4oDemoApplication.class, args);
 	}
 
